1cff20008c36a5a9e2eccb6a0e009af1dc692d4e
Vishal Maheshwari <vishalmaheshwari051@gmail.com>
2023-10-28 19:24:42 +0530

    Added Functionalities for Book and Customer Classes

--- DemoRepo/src/library/Book.java
+++ DemoRepo/src/library/Book.java
@@ -1,5 +1,32 @@
 package library;
 
+import java.util.*;
+
 public class Book {
+	
+	int id;
+	String name;
+	String author;
+	String genre;
+	
+	Book(){
+		
+	}
+	
+	Book(int id, String name, String author, String genre)
+	{
+		this.id = id;
+		this.name = name;
+		this.author = author;
+		this.genre = genre;
+	}
+	
+	void displaybook() {
+		System.out.println("--------------------------------------");
+		System.out.println("ID: "+ this.id);
+		System.out.println("Name: "+this.name);
+		System.out.println("Author: "+this.author);
+		System.out.println("Genre: "+ this.genre);
+	}
 
 }
